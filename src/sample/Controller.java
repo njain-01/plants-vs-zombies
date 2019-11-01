83d122628d9f1dcc24b11c4a346825ecef5378ae
anmolgupta <anmolgupta1311@gmail.com>
2019-11-01 20:41:41 +0530

    Init commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void buttonHandler(ActionEvent e) {
+        System.out.println("Here");
+    }
+
+    public void exitButtonHandler(ActionEvent e) {
+        System.exit(0);
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        System.out.println(System.getProperties());
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 336));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
